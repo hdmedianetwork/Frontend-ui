43c17d71dcbad0e6614a85ed0d7cb8b0363c5955
Ubuntu <work.hdmedianetwork@gmail.com>
2025-01-03 13:10:58 +0000

    add anywhere host

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Allow access from any external IP
+    port: 5173         // You can change this if you need to use a different port
+  }
 });
